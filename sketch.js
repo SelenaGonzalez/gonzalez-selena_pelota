7f6167938556f83e738f4e8ea961bfde711b59dc
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-09-25 16:56:18 -0500

    modificación de la posición de la pelota y color de fondo

--- sketch.js
+++ sketch.js
@@ -1,15 +1,49 @@
+let posX;
+let posY;
+let diametro;
+let radio;
+let fondo;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  posX = windowWidth / 2;
+  posY = windowHeight / 2;
+  // frameRate (10);
+  diametro = 100;
+  radio = diametro / 2;
+  fondo = color(25, 100, 50);
+
+  // print(posX);
+  // posX = 234;
+  // print(posX);
+  // posX = posX + 25;
+  // print(posX);
+  // posX = posX * 2;
+  // print(posX);
+  // posX = posX / 5;
+  // print(posX);
+  // posX *= 8;
+  // print(posX);
+  // posX += 1;
+  // print(posX);
+  // posX ++;
 }
 
 function draw() {
-  background(255);
-  fill(mouseX);
-  strokeWeight(mouseY);
-  circle(windowWidth / 2, windowHeight / 2, 100);
-  textSize(20);
-  text(mouseX, 50, 50);
-  text(int(mouseY), 50, 100);
-  point()
+  background(fondo);
 
+  posX -= 10;
+  if (posX < -radio) {
+    diametro = random(10, 100);
+    radio = diametro / 2;
+    fondo = color(255, random(100, 255), 50);
+    posX = windowWidth + radio;
+  }
+  posX -= 1;
+
+  fill(0);
+
+  circle(posX, posY, diametro);
+
+  print(posX);
 }
